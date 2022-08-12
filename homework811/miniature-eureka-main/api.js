4173eb1b8b29382989b5678fc0e0e3e59b27306e
Tiffany-Obrien <105178688+Tiffany-Obrien@users.noreply.github.com>
2022-08-11 21:14:39 -0400

    Add files via upload

---
+++ homework811/miniature-eureka-main/api.js
@@ -0,0 +1,24 @@
+
+const router = require('express').Router();
+
+const saveData = require('../db/saveData');
+
+// get
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// post
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+//delete as bonus?
+
+module.exports = router;
---
+++ homework811/miniature-eureka-main/html.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/note', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// no route? go to home page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+module.exports = router;
